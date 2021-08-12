90354138a0dca8f5c8deefe1259da42230b2592a
huahongjun <huahongjun1@huawei.com>
2021-08-12 17:27:00 +0800

    文档2

---
+++ docs/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="box">
+    <test/>
+  </div>
+</template>
+
+<script>
+import '../theme/src/index.scss'
+import test from './documents/test.md'
+export default {
+  components: {
+    test
+  }
+}
+</script>
