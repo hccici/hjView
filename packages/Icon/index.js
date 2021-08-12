ca8f1c5c47896fe38c3acc251a8ca381524498c5
l00501673 <liuhuijie6@huawei.com>
2021-08-12 09:47:36 +0800

    【需求/DTS单号】 【修改说明】test

--- packages/Icon/index.js
+++ packages/Icon/index.js
@@ -1,7 +1,7 @@
 import HjIcon from './HjIcon';
 // 为组件提供 install 安装方法，供按需引入
 HjIcon.setSvgSymbol = function () {
-  import('./iconfont')
+  import('./iconfont');
 }
 HjIcon.install = function (Vue) {
   Vue.component(HjIcon.name, HjIcon)
